754ec73fd421aba2e23751893672235e5ba492ac
S.M.ABTAHI NOOR <73340940+19smabtahinoor@users.noreply.github.com>
2021-04-26 20:12:12 +0600

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ addbtn.forEach(btn => {
 
         if(btn.parentNode.parentNode.classList.contains('active')){
             btn.parentNode.parentNode.classList.remove('active')
+            btn.innerHTML = '&#xe145;'
         }else{
             addbtn.forEach( newQue => {
                 newQue.parentNode.parentNode.classList.remove('active')
             })
             btn.parentNode.parentNode.classList.add('active')
-        }
+            btn.innerHTML = '&#xe15b;'
+        };
 
     })
 })
